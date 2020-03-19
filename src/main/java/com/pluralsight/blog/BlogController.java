4380cbed44ba759d201c1cf24a7e9e5a7f9eb44e
Clément DUMONT <cdumont@partner.auchan.fr>
2020-03-19 13:38:46 +0100

    feat: module2 display posts title

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,27 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    public PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap model){
+        List<Post> allPosts = postRepository.getAllPosts();
         model.put("title", "Blog Post 1");
+        model.put("posts", allPosts);
         return "home";
     }
 
